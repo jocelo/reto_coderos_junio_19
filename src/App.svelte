7693d574622558b05aa47cef6d5128568648bcea
jocelo <gopher@gmail.com>
2019-06-15 00:24:02 -0400

    adding card validator

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,40 @@
+<style>
+	p {
+		color: purple;
+		font-family: 'Comic Sans MS';
+		font-size: 2em;
+	}
+
+	.container {
+		width: 50%;
+		margin: auto;
+	}
+</style>
 <script>
-	let name = 'mundo de coderos',
-		iteration = 'second';
+	import Nested from './Nested.svelte';
+	import Card from './Card.svelte';
+	import AddForm from './AddForm.svelte';
+
+	const name = 'fff';
+	const src='https://homepages.cae.wisc.edu/~ece533/images/airplane.png';
+	let count = 0;
+	let doubled = 0;
+	$: {
+		console.log(`the count is: ${count}`);
+		doubled = count * 2;
+	}
+
+	$: if (count == 9) {
+		alert('wachaoffffut !!');
+	}
+
+	function handleClick() {
+		count ++;
+	}
 </script>
 
-<h1>Hello {name}!</h1>
-<h2>Testing {iteration} run</h2>
+<div class="container">
+	<h2>Agregar Nuevo Metodo de Pago</h2>
+	<Card />
+	<AddForm />
+</div>
