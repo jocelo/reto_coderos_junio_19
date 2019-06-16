c298060f6316d17e1ab93d288f868842bb8a4519
jocelo <gopher@gmail.com>
2019-06-16 07:58:37 -0400

    code cleanup

--- src/App.svelte
+++ src/App.svelte
@@ -1,36 +1,12 @@
 <style>
-	p {
-		color: purple;
-		font-family: 'Comic Sans MS';
-		font-size: 2em;
-	}
-
 	.container {
 		width: 50%;
 		margin: auto;
 	}
 </style>
 <script>
-	import Nested from './Nested.svelte';
 	import Card from './Card.svelte';
 	import AddForm from './AddForm.svelte';
-
-	const name = 'fff';
-
-	let count = 0;
-	let doubled = 0;
-	$: {
-		console.log(`the count is: ${count}`);
-		doubled = count * 2;
-	}
-
-	$: if (count == 9) {
-		alert('wachaoffffut !!');
-	}
-
-	function handleClick() {
-		count ++;
-	}
 </script>
 
 <div class="container">
